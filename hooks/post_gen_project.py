ba9c3ee738a7cd591124df45bfa9b52b4021430c
Danil Akhtarov <daxartio@gmail.com>
2022-12-04 16:07:52 +0300

    feat: add use_docs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os
+import shutil
 
 USE_MAKEFILE = "{{ cookiecutter.task_runner }}" == 'Makefile'
+USE_DOCS = "{{ cookiecutter.use_docs }}" == 'True'
 LICENSE = "{{ cookiecutter.license }}"
 USE_MIT = LICENSE == 'MIT'
 USE_APACHE = LICENSE == 'APACHE'
@@ -10,6 +12,10 @@ USE_APACHE = LICENSE == 'APACHE'
 def main():
     if not USE_MAKEFILE:
         os.remove('Makefile')
+    if not USE_DOCS:
+        os.remove('mkdocs.yml')
+        os.remove('.github/workflows/github_pages.yml')
+        shutil.rmtree('docs')
     if not USE_MIT:
         os.remove('LICENSE-MIT')
     if not USE_APACHE:
