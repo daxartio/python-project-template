6a237bdadd18ea1d685e610e31afe28fecbb17cc
Danil Akhtarov <daxartio@gmail.com>
2023-02-02 00:03:52 +0300

    feat: simplify linters

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/cli/app.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/cli/app.py
@@ -5,9 +5,10 @@ from {{cookiecutter.pkg_name}} import __version__
 
 def _parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        prog='{{ cookiecutter.project_slug }}', description=f'{{ cookiecutter.project_slug }} {__version__}'
+        prog="{{ cookiecutter.project_slug }}",
+        description=f"{{ cookiecutter.project_slug }} {__version__}",
     )
-    parser.add_argument('-V', '--version', action='version', version=__version__)
+    parser.add_argument("-V", "--version", action="version", version=__version__)
 
     return parser.parse_args()
 
